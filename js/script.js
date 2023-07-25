33f536d5f4e484812f2ece6da5c00acbdb1e46ec
zhaleh <zhalehelmi@gmail.com>
2023-07-25 13:12:47 +0330

    getcookie

--- js/script.js
+++ js/script.js
@@ -8,25 +8,13 @@ function setCookie(cookieName, cookieValue, exDay) {
   document.cookie = `${cookieName}=${cookieValue};path=/;expires=${now}`;
 }
 
-getCookieBtn.addEventListener("click", () => {
-  let mainCookieName = prompt("Enter the name");
-
+function getCookie(cookieName) {
   let cookiesArray = document.cookie.split(";");
-  console.log(cookiesArray);
   let mainCookie = null;
   cookiesArray.some((cookie) => {
-    if (cookie.includes(mainCookieName)) {
-      mainCookie = cookie.substring(cookie.indexOf("=") + 1);
-      return true;
-    }
+    mainCookie = cookie.substring(cookie.indexOf("=") + 1);
+    return true;
   });
-  console.log(mainCookie);
-});
-removeCookieBtn.addEventListener("click", () => {
-  console.log("ok");
-  let mainCookieName = prompt("Enter the main cookie name");
-  const now = new Date();
-  now.setTime(now.getTime() - 2 * 24 * 60 * 60 * 1000);
-  console.log(now);
-  document.cookie = `${mainCookieName}=28;path=/;expries=${now}`;
-});
+
+  return mainCookie;
+}
